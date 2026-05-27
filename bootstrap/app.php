d0743b7d36efb236cb45feea987488e6522926fd
mnabizar <mnazz71117@gmail.com>
2026-05-28 01:18:00 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
 
         $middleware->web(append: [
