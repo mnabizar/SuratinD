7c6052384f90f64903cc7070f884ce4d7e2a904e
mnabizar <mnazz71117@gmail.com>
2026-05-28 14:52:35 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'session.timeout' => \App\Http\Middleware\SessionTimeout::class,
         ]);
 
         $middleware->web(append: [
+            \App\Http\Middleware\SessionTimeout::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
